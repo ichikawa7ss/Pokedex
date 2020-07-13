b56ea1f7e450b2914bc055cf02040ec4b91456d8
ichikawa7ss <i2s7.chacha@gmail.com>
2020-07-13 20:26:34 +0900

    Add WatchOSApp and WatchAppExtension target

---
+++ WatchOSApp Extension/InterfaceController.swift
@@ -0,0 +1,29 @@
+//
+//  InterfaceController.swift
+//  WatchOSApp Extension
+//
+//  Created by ichikawa on 2020/07/09.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
